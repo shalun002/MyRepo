10d577e54406aea6a9ed725faeeb07b907cd1a4d
shalun002 <32149071+shalun002@users.noreply.github.com>
2017-11-23 10:08:41 +0600

    Metod

---
+++ Project2/Metod.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Методы класса
+Создание метода класса*/
+
+class Human {    // Создаем класс Human
+
+public:						// модификатор доступа публичный
+
+	string name;
+	int age;				// поле  класса с целочисленным типом, которые видны только в этом классе
+	int weight;
+
+	void Print() {				// Метод класса Human
+		cout << " Имя: " << name << " " << " Вес: " << weight << " " << " Возраст: " << age << endl;
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	Human human;         // Создаем объект класса
+	human.age = 25;		// инициализация полей класса
+	human.name = "Afanasyi";
+	human.weight = 58;
+
+	human.Print();  //вызов метода(функции) класса Human
+	system("pause");
+	return 0;
+}
