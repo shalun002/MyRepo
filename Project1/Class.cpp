3df0b5a68ebeaca0cd9c20a7e9d8217fa787c6bc
shalun002 <32149071+shalun002@users.noreply.github.com>
2017-11-23 08:17:20 +0600

    Metod

--- Project1/Class.cpp
+++ Project1/Class.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-/*Создание класса
-  Создание объекта класса*/
-
-class MyClass {    // Создаем класс MyClass
-public:						// модификатор доступа публичный
-	int age;				// поле  класса с целочисленным типом
-	string name;
-
-};
-
-
-
-int main() {
-	setlocale(LC_ALL, "ru");
-
-	MyClass cl;         // Создаем объект класса
-
-	cl.age = 25;		// инициализация полей класса
-	cl.name = "Afanasyi";
-
-	cout << cl.age << " " << cl.name << endl;  //вывод значений на консоль
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+///*Создание класса
+//  Создание объекта класса*/
+//
+//class MyClass {    // Создаем класс MyClass
+//public:						// модификатор доступа публичный
+//	int age;				// поле  класса с целочисленным типом
+//	string name;
+//
+//};
+//
+//int main() {
+//	setlocale(LC_ALL, "ru");
+//
+//	MyClass cl;         // Создаем объект класса
+//
+//	cl.age = 25;		// инициализация полей класса
+//	cl.name = "Afanasyi";
+//
+//	cout << cl.age << " " << cl.name << endl;  //вывод значений на консоль
+//
+//	system("pause");
+//	return 0;
+//}
