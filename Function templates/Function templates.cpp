ba0894ce728663eeeed5709e5b9631937867bf9d
Sergey <Sergey@SERGEY-PC>
2017-11-26 19:08:53 +0600

    Function templates

--- Function templates/Function templates.cpp
+++ Function templates/Function templates.cpp
@@ -10,22 +10,22 @@ int Sum(int a, int b, int c) {
 	return a + b + c;
 }
 
-int Sum(int a, int b) {
+int Sum(int a, int b) {			// Перегрузка функции с типом int
 	return a + b;
 }
 
-double Sum(double a, double b) {
+double Sum(double a, double b) {    // Перегрузка функции с типом double
 	return a + b;
 }
 
-float Sum(float a, float b, float c) {
+float Sum(float a, float b, float c) {  // Перегрузка функции с типом float
 	return a + b + c;
 }
 
 int main() {
 	setlocale(LC_ALL, "ru");
 
-	cout << Sum(5.3f, 2.2f, 5.6f) << endl;
+	cout << Sum(5.3f, 2.2f, 5.6f) << endl; // Вывод фунции на консоль
 	system("pause");
 	return 0;
 }
