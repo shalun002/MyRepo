ae03f2bdb1764a314f99d63df810233c0452a7f1
Sergey <Sergey@SERGEY-PC>
2017-11-26 20:07:19 +0600

    function template

--- Function templates/Function templates.cpp
+++ Function templates/Function templates.cpp
@@ -3,29 +3,37 @@
 using namespace std;
 
 
-/*Шаблоны функций. Шаблонные функции c++. template typename. template class.*/
+/*Шаблоны функций. Шаблонные функции c++.*/
 
+//int Sum(int a, int b, int c) {
+//	return a + b + c;
+//}
+//
+//int Sum(int a, int b) {			// Перегрузка функции с типом int
+//	return a + b;
+//}
+//
+//double Sum(double a, double b) {    // Перегрузка функции с типом double
+//	return a + b;
+//}
+//
+//float Sum(float a, float b, float c) {  // Перегрузка функции с типом float
+//	return a + b + c;
+//}
 
-int Sum(int a, int b, int c) {
-	return a + b + c;
-}
-
-int Sum(int a, int b) {			// Перегрузка функции с типом int
+template <typename T1, typename T2>  // шаблонная функция с двумя разными параметрами "Т1" и "Т2". т.е. можно передавать разные типы данных.
+T1 Sum(T1 a, T2 b) {	
 	return a + b;
-}
-
-double Sum(double a, double b) {    // Перегрузка функции с типом double
-	return a + b;
-}
-
-float Sum(float a, float b, float c) {  // Перегрузка функции с типом float
-	return a + b + c;
 }
 
 int main() {
 	setlocale(LC_ALL, "ru");
 
-	cout << Sum(5.3f, 2.2f, 5.6f) << endl; // Вывод фунции на консоль
+	cout << Sum(5, 2) << endl; // Вывод фунции на консоль
+
+	cout << Sum(5.2, 2) << endl; // Вывод фунции на консоль
+
+
 	system("pause");
 	return 0;
 }
