c43f99b832f7d7a3588016c42129e9a0c452f3a5
shalun002 <32149071+shalun002@users.noreply.github.com>
2017-11-27 10:16:28 +0600

    template

--- Home_Work_Template/Template.cpp
+++ Home_Work_Template/Template.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
 
 
@@ -10,6 +8,32 @@ using namespace std;
 переданное значение.
 */
 
+template <typename T>  // шаблонная функция .
+T FuncArrMin(T *arr[], int size) {
+	if (size < 1) {
+		cout << "array is empty!" << endl;
+	}
+	T min = arr[0];
+	for (int i = 0; i < size; ++i) {
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	return min;
+}
+
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+
+	int arr[10] = { 2, 3, 7, 9, 6, 3, 1, 5, 2, 8 };
+
+	cout << FuncArrMin << endl;
+
+	system("pause");
+	return 0;
+}
 
 
 
