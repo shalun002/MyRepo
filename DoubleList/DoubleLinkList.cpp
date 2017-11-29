2b78b852fb6aa2113dbfceb6bfa40782aaf1ee6f
БлиновС <@223_15>
2017-11-29 19:36:35 +0600

    DoubleLinkList

---
+++ DoubleList/DoubleLinkList.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "DoubleLinkList.h"
+
+// реализация фукции класса
+template<typename T>
+DoubleLinkList::DoubleLinkList()
+{
+}
+
+template<typename T>
+DoubleLinkList::~DoubleLinkList()
+{
+}
---
+++ DoubleList/DoubleLinkList.h
@@ -0,0 +1,12 @@
+#pragma once
+
+//описание класса двусвязный список и прототипы его функций
+template<typename T>
+class DoubleLinkList
+{
+public:
+	//пустой конструктор для создания двусвязного списка
+	DoubleLinkList();
+	~DoubleLinkList();
+};
+
