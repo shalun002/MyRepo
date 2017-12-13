cb211a577646b48c088d222adc3a948c45a04fdc
БлиновС <@223_15>
2017-12-13 21:15:58 +0600

    type conversion

--- Type conversion/Type conversion.cpp
+++ Type conversion/Type conversion.cpp
@@ -1,12 +1,16 @@
 #include <typeinfo>
 #include <iostream>
 
-class A{};
+class A
+{
+public:
+	A(){}
+	virtual ~A(){}
+};
 class B : public A{};
 
-int main() {
-	setlocale(LC_ALL, "ru");
-
+void fi() 
+{
 	A a;
 	B b;
 
@@ -16,6 +20,37 @@ int main() {
 	std::cout << "\n";
 	std::cout << typeid(a).name();
 	std::cout << "\n";
+
+}
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+
+	A *a = new B();
+	B *b = dynamic_cast<B *> (new A());
+
+	double y = 2.5;
+	int x = static_cast <int> (y);
+
+	const double q = 5.5;
+	double *w = const_cast<double *>(&q);
+
+	//double *c = reinterpret_cast<double *>(123);
+
+	std::cout << x;
+	std::cout << "\n";
+
+	std::cout << *w;
+	std::cout << "\n";
+
+	//std::cout << *c;
+	//std::cout << "\n";
+	
+	//delete c;
+	delete a;
+	delete b;
+	delete w;
 
 	system("pause");
 	return 0;
