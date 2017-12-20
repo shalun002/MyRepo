2d659cab4b12ada222cc1d4d7a1629c0b399a679
БлиновС <@223_15>
2017-12-20 20:16:04 +0600

    MultiMap

--- 20.12.2017/20.12.2017.cpp
+++ 20.12.2017/20.12.2017.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <list>
 #include <string>
+#include <algorithm>
 
 
 void Vector() {
@@ -18,10 +19,12 @@ void Vector() {
 	std::cout << "Vector" << std::endl;
 
 }
+
 void Map() {
 	std::map <std::string, int> dictionary;
 	dictionary.emplace(std::make_pair<std::string, int>("123", 1));
 	std::cout << dictionary.at("123") << std::endl;
+	dictionary.emplace(std::make_pair<std::string, int>("123", 1));
 	auto it2 = dictionary.begin();
 	for (; it2 != dictionary.end(); ++it2) {
 		std::cout << it2->first << '\t' << it2->second << '\t';
@@ -30,10 +33,25 @@ void Map() {
 	std::cout << "Map" << std::endl;
 	std::cout << std::endl;
 }
+
+void MultiMap() {
+	std::multimap <std::string, int> dictionary1;
+	dictionary1.insert(std::make_pair<std::string, int>("123", 1));
+	std::cout << dictionary1.find("123")->first << std::endl;
+	dictionary1.insert(std::make_pair<std::string, int>("123", 5));
+	auto it3 = dictionary1.begin();
+	for (; it3 != dictionary1.end(); ++it3) {
+		std::cout << it3->first << '\t' << it3->second << '\t';
+	}
+	std::cout << std::endl;
+	std::cout << "Multimap" << std::endl;
+	std::cout << std::endl;
+}
+
 void List() {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
 	std::list <int> y(arr, arr + 10);
-	y.sort();     // сортировка массива раз
+	y.sort();     // сортировка массива
 	auto it1 = y.begin();
 	for (; it1 != y.end(); ++it1)
 	{
@@ -43,15 +61,17 @@ void List() {
 	std::cout << "List" << std::endl;
 	std::cout << std::endl;
 }
+
 int main() {
 	setlocale(LC_ALL, "ru");
+
 	Vector();
 	
 	List();
 	
 	Map();
 	
-
+	MultiMap();
 
 
 	system("pause");
