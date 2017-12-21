a1089e8854595a7d4bffb9f420f76aa3a265c4b2
БлиновС <@223_15>
2017-12-21 19:39:48 +0600

    lambda

--- 21.12.2017/21.12.2017.cpp
+++ 21.12.2017/21.12.2017.cpp
@@ -5,10 +5,37 @@
 #include <string>
 #include <algorithm>
 
+class A {
+public:
+
+	A() {
+		std::cout << "A()" << std::endl;
+	}
+	A(int x) :value(x) {}
+
+	~A() {
+		std::cout << "~A()" << std::endl;
+	}
+
+	int value;
+
+	friend std::ostream& operator << (std::ostream &, const A &);
+	friend void operator >> (std::istream & stream, const A & a);
+};
+
+std::ostream & operator<<(std::ostream & stream, const A & a)
+{
+	return stream << a.value;
+}
+
+void operator >> (std::istream & stream, const A & a)
+{
+	stream >> a;
+}
 
 int main() {
 
-	std::vector<A>;
+	std::vector<A> x = { A(1), A(2), A(3) };
 
 	system("pause");
 	return 0;
