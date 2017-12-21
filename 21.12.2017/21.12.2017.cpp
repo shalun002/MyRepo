8372932f509dffdcaf110c60b178d9e99a6294ae
БлиновС <@223_15>
2017-12-21 19:26:01 +0600

    lambda

---
+++ 21.12.2017/21.12.2017.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <string>
+#include <algorithm>
+
+
+int main() {
+
+	std::vector<A>;
+
+	system("pause");
+	return 0;
+}
