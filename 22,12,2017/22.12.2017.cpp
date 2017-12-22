48b97f1f23629b4a05b1415a0f8eabfa02bfd817
БлиновС <@223_15>
2017-12-22 21:24:23 +0600

    vector add & delete & sort

--- 22,12,2017/22.12.2017.cpp
+++ 22,12,2017/22.12.2017.cpp
@@ -24,33 +24,38 @@ int main() {
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "Sortirovka po ubivaniyu      "; std::sort(x.begin(), x.end(), std::greater<int>());
+	std::cout << "Sortirovka po ubivaniyu      " << std::endl; std::sort(x.begin(), x.end(), std::greater<int>());  // Sort 9 - 0
 	std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });	
 	std::cout << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "Sortirovka po vozrastaniyu   "; std::sort(x.begin(), x.end(), std::less<int>());
+	std::cout << "Sortirovka po vozrastaniyu   " << std::endl; std::sort(x.begin(), x.end(), std::less<int>());  // Sort 0 - 9
 	std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
 	std::cout << std::endl;
 	std::cout << std::endl;
 	
 	int n = 2;
-	std::cout << "Uvelicit na n-oe chislo:   "; std::for_each(x.begin(), x.end(), [n](int &a) { a += n; });
+	std::cout << "Uvelicit na n-oe chislo:   " << std::endl; std::for_each(x.begin(), x.end(), [n](int &a) { a += n; });
 	std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
 	std::cout << std::endl;
 
 	std::cout << std::endl;
-	std::cout << "Umenshit na n-oe chislo:  "; std::for_each(x.begin(), x.end(), [n](int &a) { a -= n; });
+	std::cout << "Umenshit na n-oe chislo:  " << std::endl; std::for_each(x.begin(), x.end(), [n](int &a) { a -= n; });
 	std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
 	std::cout << std::endl;
 	
 	std::cout << std::endl;
 
 	x.erase(x.begin() + n);
-	std::cout << "Udalenie elementa iz massiva "; std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
+	std::cout << "Udalenie elementa iz massiva " << std::endl; std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
 	std::cout << std::endl;
 	std::cout << std::endl;
 
+	std::cout << std::endl;
+	x.erase(std::remove(x.begin(), x.end(), 4), x.end());
+	std::cout << "affter 4 is removed: " << std::endl;
+	std::for_each(x.begin(), x.end(), [](int &a) { std::cout << a << " "; });
+	std::cout << std::endl;
 	system("pause");
 	return 0;
 }
