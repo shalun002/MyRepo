73eda1092e9f456c0ceeb4d6e6105fad1c9107da
Sergey <Sergey@SERGEY-PC>
2017-12-23 22:44:53 +0600

    HW_1512397076

--- HomeWork_1513177572/HomeWork_1513177572.cpp
+++ HomeWork_1513177572/HomeWork_1513177572.cpp
@@ -1,5 +1,6 @@
+#include <tchar.h>
 #include <iostream>
-#include<string>
+#include <string>
 
 
 /*В рамках предыдущих самостоятельных и домашних заданий вы создали набор классов по работе со структурами данных
@@ -7,8 +8,79 @@
 пространств.Используя механизм пространств напишите код по тестированию полученного решения.*/
 
 
-int main() {
 
+namespace firstNameSpace 
+{
+	class Human
+	{
+	public:
+		std::string name;
+		int age;
+		std::string city;
+
+		Human(std::string name)
+		{
+			this->name = name;
+			std::cout << "Name: " << name << std::endl;
+			std::cout << std::endl;
+		}
+
+		static void addInfo(std::string city, int age)
+		{
+			
+			std::cout << "\t\tInformation about a person" << std::endl;
+			std::cout << std::endl;
+			std::cout << "City: " << city << std::endl;
+			std::cout << "Age: " << age << std::endl;
+			
+				
+		}
+
+		 Human() {}
+		~Human() {}	
+	};
+
+}
+
+namespace secondNameSpace 
+{
+	class Animal
+	{
+	public:
+		std::string name;
+		int age;
+		std::string city;
+
+		Animal(std::string name)
+		{
+			this->name = name;
+			std::cout << "Name:" << name << std::endl;
+			std::cout << std::endl;
+		}
+
+		static void addInfo(std::string city, int age)
+		{
+
+			std::cout << "\t\tInformation about a animal: " << std::endl;
+			std::cout << std::endl;
+			std::cout << "City: " << city << std::endl;
+			std::cout << "Age: " << age << std::endl;
+		}
+		 Animal() {}
+		~Animal() {}
+	};
+
+}
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	
+	firstNameSpace::Human::addInfo("Kazakstan", 23);
+	firstNameSpace::Human::Human("Oleg");
+
+	secondNameSpace::Animal::addInfo("Australia", 2);
+	secondNameSpace::Animal::Animal("Kangaroo");
 
 	system("pause");
 	return 0;
