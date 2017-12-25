47c03336793c4f07e37fea457a88bdaf9c75508b
БлиновС <@223_15>
2017-12-25 20:52:07 +0600

    25.12.2017

--- 25.12.2017/25.12.2017.cpp
+++ 25.12.2017/25.12.2017.cpp
@@ -3,10 +3,34 @@
 #include <string>
 
 
+class Singletone {
+
+public:
+	static Singletone &getInstance() 
+	{
+		static Singletone instance;
+		return instance;
+	}
+
+private:
+	Singletone(){}
+	Singletone(const Singletone &) = delete;
+	Singletone & operator = (const Singletone &) = delete;
+
+};
+
 
 
 int main() {
 	setlocale(LC_ALL, "ru");
+	
+	Singletone& a = Singletone::getInstance();
+	Singletone& b = Singletone::getInstance();
+	Singletone& c = Singletone::getInstance();
+
+	std::cout << &a << std::endl;
+	std::cout << &b << std::endl;
+	std::cout << &c << std::endl;
 
 
 	system("pause");
